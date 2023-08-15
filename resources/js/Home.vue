70e2ad45b444846d9643490d7dc4c931d0f0c0d7
razmanarif <razmanarif@gmail.com>
2023-08-15 13:51:40 +0800

    added dark mode button

--- resources/js/Home.vue
+++ resources/js/Home.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="bg-cyan-400 min-h-screen flex items-center justify-center">
-        <div class="bg-white w-72 h-96 rounded-lg shadow-lg">
-            <!-- Added shadow for better distinction -->
+    <div :class="isDarkMode ? 'bg-gray-900 text-white min-h-screen flex items-center justify-center' : 'bg-cyan-400 min-h-screen flex items-center justify-center'">
+        <!-- <button @click="toggleDarkMode" class="absolute top-4 right-4 px-4 py-2 bg-green-500 rounded-full">Toggle Dark Mode</button> -->
+        <div :class="isDarkMode ? 'bg-gray-800 w-72 h-96 rounded-lg shadow-lg' : 'bg-white w-72 h-96 rounded-lg shadow-lg'">
             <img
                 :src="photoURL"
                 alt="Profile Picture"
@@ -10,23 +10,27 @@
             <div class="p-6">
                 <h1 class="text-xl font-bold">{{ personName }}</h1>
                 <p class="mt-2 text-xs">{{ personDescription }}</p>
-                <!-- <font-awesome-icon icon="fa-solid fa-user-secret" /> -->
                 <div class="flex space-x-4 mt-4">
+                    <!-- The code for your social icons remains unchanged -->
+                    <!-- Twitter -->
                     <div class="bg-gray-200 rounded-full p-2 w-6 h-6 flex items-center justify-center">
                         <a :href="twitterURL" target="_blank">
                             <font-awesome-icon icon="fa-brands fa-twitter" class="text-sm" />
                         </a>
                     </div>
+                    <!-- Instagram -->
                     <div class="bg-gray-200 rounded-full p-2 w-6 h-6 flex items-center justify-center">
                         <a :href="instagramURL" target="_blank">
                             <font-awesome-icon icon="fa-brands fa-instagram"  class="text-sm" />
                         </a>
                     </div>
+                    <!-- Facebook -->
                     <div class="bg-gray-200 rounded-full p-2 w-6 h-6 flex items-center justify-center">
                         <a :href="facebookURL" target="_blank">
                             <font-awesome-icon icon="fa-brands fa-facebook"  class="text-sm" />
                         </a>
                     </div>
+                    <!-- LinkedIn -->
                     <div class="bg-gray-200 rounded-full p-2 w-6 h-6 flex items-center justify-center">
                         <a :href="linkedinURL" target="_blank">
                             <font-awesome-icon icon="fa-brands fa-linkedin"  class="text-sm" />
@@ -34,6 +38,7 @@
                     </div>
                 </div>
             </div>
+            <button @click="toggleDarkMode" class="mt-4 px-4 py-2 bg-green-500 rounded-full">{{ buttonText }}</button>
         </div>
     </div>
 </template>
@@ -42,6 +47,7 @@
 export default {
     data() {
         return {
+            isDarkMode: false,
             photoURL:
                 "https://plus.unsplash.com/premium_photo-1691960547805-7143654bd06b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1332&q=80",
             personName: "Maggie May",
@@ -53,5 +59,19 @@ export default {
             linkedinURL: "https://linkedin.com",
         };
     },
+    computed: {
+        buttonText() {
+            return this.isDarkMode ? 'Light Mode' : 'Dark Mode';
+        }
+    },
+    methods: {
+        toggleDarkMode() {
+            this.isDarkMode = !this.isDarkMode;
+        }
+    }
 };
 </script>
+
+<style scoped>
+/* Additional styles (if any) can be added here */
+</style>
