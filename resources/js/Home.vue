fb7b033c27689dccce0bf8c20947424487047656
razmanarif <razmanarif@gmail.com>
2023-08-15 13:15:01 +0800

    added social media icons and link

--- resources/js/Home.vue
+++ resources/js/Home.vue
@@ -1,34 +1,57 @@
 <template>
     <div class="bg-cyan-400 min-h-screen flex items-center justify-center">
-      <div class="bg-white w-72 h-96 rounded-lg shadow-lg"> <!-- Added shadow for better distinction -->
-        <img :src="photoURL" alt="Profile Picture" class="w-full h-1/2 object-cover">
-        <div class="p-4">
-          <h1 class="text-xl font-bold">{{ personName }}</h1>
-          <p class="mt-2 text-xs">{{ personDescription }}</p>
-          <div class="flex space-x-4 mt-4">
-            <a :href="twitterURL" target="_blank"><font-awesome-icon icon="twitter" /></a>
-            <a :href="instagramURL" target="_blank"><font-awesome-icon icon="instagram" /></a>
-            <a :href="facebookURL" target="_blank"><font-awesome-icon icon="facebook" /></a>
-            <a :href="linkedinURL" target="_blank"><font-awesome-icon icon="linkedin" /></a>
-          </div>
+        <div class="bg-white w-72 h-96 rounded-lg shadow-lg">
+            <!-- Added shadow for better distinction -->
+            <img
+                :src="photoURL"
+                alt="Profile Picture"
+                class="w-full h-1/2 object-cover rounded-t-lg"
+            />
+            <div class="p-6">
+                <h1 class="text-xl font-bold">{{ personName }}</h1>
+                <p class="mt-2 text-xs">{{ personDescription }}</p>
+                <!-- <font-awesome-icon icon="fa-solid fa-user-secret" /> -->
+                <div class="flex space-x-4 mt-4">
+                    <div class="bg-gray-200 rounded-full p-2 w-6 h-6 flex items-center justify-center">
+                        <a :href="twitterURL" target="_blank">
+                            <font-awesome-icon icon="fa-brands fa-twitter" class="text-sm" />
+                        </a>
+                    </div>
+                    <div class="bg-gray-200 rounded-full p-2 w-6 h-6 flex items-center justify-center">
+                        <a :href="instagramURL" target="_blank">
+                            <font-awesome-icon icon="fa-brands fa-instagram"  class="text-sm" />
+                        </a>
+                    </div>
+                    <div class="bg-gray-200 rounded-full p-2 w-6 h-6 flex items-center justify-center">
+                        <a :href="facebookURL" target="_blank">
+                            <font-awesome-icon icon="fa-brands fa-facebook"  class="text-sm" />
+                        </a>
+                    </div>
+                    <div class="bg-gray-200 rounded-full p-2 w-6 h-6 flex items-center justify-center">
+                        <a :href="linkedinURL" target="_blank">
+                            <font-awesome-icon icon="fa-brands fa-linkedin"  class="text-sm" />
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
+</template>
+
+<script>
+export default {
     data() {
-      return {
-        photoURL: "https://plus.unsplash.com/premium_photo-1691960547805-7143654bd06b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1332&q=80",
-        personName: "Maggie May",
-        personDescription: "I'm a designer from Amsterdam, the Netherlands. Early 2017 I joined Framer to help build the next generation interactive design tool. At framer I help shape the brand and build Framer.com",
-        twitterURL: "https://twitter.com/johndoe",
-        instagramURL: "https://instagram.com/johndoe",
-        facebookURL: "https://facebook.com/johndoe",
-        linkedinURL: "https://linkedin.com/in/johndoe"
-      };
-    }
-  }
-  </script>
-  
+        return {
+            photoURL:
+                "https://plus.unsplash.com/premium_photo-1691960547805-7143654bd06b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1332&q=80",
+            personName: "Maggie May",
+            personDescription:
+                "I'm a designer from Amsterdam, the Netherlands. Early 2017 I joined Framer to help build the next generation interactive design tool. At framer I help shape the brand and build Framer.com",
+            twitterURL: "https://twitter.com",
+            instagramURL: "https://instagram.com",
+            facebookURL: "https://facebook.com",
+            linkedinURL: "https://linkedin.com",
+        };
+    },
+};
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -2,5 +2,13 @@ import './bootstrap';
 import Home from './Home.vue';
 
 import {createApp} from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faTwitter, faInstagram, faFacebook, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
-createApp(Home).mount("#app");
+
+library.add(faTwitter, faInstagram, faFacebook, faLinkedin);
+
+createApp(Home)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount("#app");
